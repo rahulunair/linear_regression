612aa5bf634af4f24bbac9c42ddd950d671ae5ae
rahulunair <rahulunair@gmail.com>
2020-02-21 00:50:24 -0800

    init zig

--- zig/lr.zig
+++ zig/lr.zig
@@ -13,7 +13,7 @@
 //               straight line through the data points.
 //
 //  Files: Source files:
-//          lr.py                       Main program
+//          lr.zig                     Main program
 //
 //
 //***************************************************************
@@ -22,8 +22,21 @@ const std = @import("std");
 const os = std.os;
 const assert = std.debug.assert;
 
+const end = "end";
+
+var n: u32 = 0;
+var sum_x: f32  = 0.0;
+var sum_xx: f32 = 0.0;
+var sum_y: f32  = 0.0;
+var sum_yy: f32 = 0;
+var sum_xy: f32 = 0.0;
+var data_entry: bool = true;
+
 pub fn main() void {
     warn("\nLR - perform linear regeression");
     warn("\nenter 'end' to stop data entry and compute LR.");
     warn("\n\n");
+    warn("{}", sum_x);
+
+    
 }
