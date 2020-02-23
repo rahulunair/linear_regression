bbd9675c01f80e09b41b898f704d83a720339112
rahulunair <rahulunair@gmail.com>
2020-02-22 17:11:19 -0800

    fixed few bugs

--- zig/lr.zig
+++ zig/lr.zig
@@ -24,13 +24,13 @@
 const std = @import("std");
 const stdin = std.io.getStdIn();
 const warn = std.debug.warn;
+const fmt = std.fmt;
 //***************************************************************
 
 //***************************************************************
 //  global variables
 //***************************************************************
-var input: [20]u8 = undefined;
-var n: f32 = 0;
+var n: u16 = 0;
 var x: f32 = 0.0;
 var y: f32 = 0.0;
 var sum_x: f32 = 0.0;
@@ -42,32 +42,49 @@ var sum_xy: f32 = 0.0;
 
 pub fn main() !void {
     warn("\nLR - perform linear regeression", .{});
-    warn("\n\n", .{});
-    warn("How many (x, y) pairs do you have? ", .{});
-    const num = try stdin.read(&input);
-    warn("Number of pairs {}", .{num});
-    while (@floatToInt(u8, n) <= num) {
-        warn("\nloop number: {}", .{@floatToInt(u8, n)});
-        n += 1;
-        var buffer: [50]u8 = undefined;
+    warn("\nHow many (x, y) pairs? ", .{});
+    var num_buf: [20]u8 = undefined;
+    const amt = try stdin.read(&num_buf);
+    const line = std.mem.trimRight(u8, num_buf[0..amt], " \r\n\t");
+    while (true) {
+        const num = std.fmt.parseUnsigned(u16, line, 10) catch {
+            continue;
+        };
+        if (n == num) {
+            break;
+        }
+        n += 1; // loop counter
+        warn("\nPair: {}", .{n});
         warn("\nEnter x: ", .{});
-        x = @intToFloat(f32, try stdin.read(&buffer));
+        var x_buf: [20]u8 = undefined;
+        const x_len = try stdin.read(&x_buf);
+        const x_line = std.mem.trimRight(u8, x_buf[0..x_len], " \r\n\t");
+        x = std.fmt.parseFloat(f32, x_line) catch {
+            continue;
+        };
+
         warn("Enter y: ", .{});
-        y = @intToFloat(f32, try stdin.read(&buffer));
+        var y_buf: [20]u8 = undefined;
+        const y_len = try stdin.read(&y_buf);
+        const y_line = std.mem.trimRight(u8, y_buf[0..y_len], " \r\n\t");
+        y = std.fmt.parseFloat(f32, y_line) catch {
+            continue;
+        };
         sum_x += x;
         sum_xx += x * x;
         sum_y += y;
         sum_yy += y * y;
         sum_xy += x * y;
     }
-    warn("total n {}", .{n});
-    const m = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - @exp2(sum_x));
-    const b = (sum_y * sum_xx - sum_x * sum_xy) / (n * sum_xx - @exp2(sum_x));
-    const r = (sum_xy - sum_x * sum_y / n) / @sqrt((sum_xx - @exp2(sum_x) / n) * (@exp2(sum_y) / 2));
+    warn("\n]- total N = {}", .{n});
+    const N = @intToFloat(f32, n);
+    const m = (N * sum_xy - sum_x * sum_y) / (N * sum_xx - @exp2(sum_x));
+    const b = (sum_y * sum_xx - sum_x * sum_xy) / (N * sum_xx - @exp2(sum_x));
+    const r = (sum_xy - sum_x * sum_y / N) / @sqrt((sum_xx - @exp2(sum_x) / N) * (@exp2(sum_y) / 2));
     //**************************************************************
     //  print results
     //**************************************************************
-    warn("\n] slope m = {}", .{m});
-    warn("\n] y-intercept b = {}", .{b});
-    warn("\n] correlation r = {}", .{r});
+    warn("\n]- slope m = {}", .{m});
+    warn("\n]- y-intercept b = {}", .{b});
+    warn("\n]- correlation r = {}\n", .{r});
 }
